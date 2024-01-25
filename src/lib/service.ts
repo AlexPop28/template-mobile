ae685ea4d98c56e48fb68cb1fde47a1459c56113
Alex Pop <popalexnicolae@gmail.com>
2024-01-25 08:28:25 +0200

    Update logs

--- src/lib/service.ts
+++ src/lib/service.ts
@@ -28,16 +28,17 @@ const removeOfflineKeys = (obj: Model) => {
 };
 const getAll = async (setIsLoading: setIsLoadingT): Promise<Model[]> => {
   const axios = useAxios();
-  console.log("SERVER: trying getAll");
+  const funcName = `getAll(${category_name})`;
+  console.log(`SERVER: trying ${funcName}`);
 
   const request = async () => {
     try {
       const { data } = await axios.get("/pets");
-      console.log("SERVER: getAll successful");
+      console.log(`SERVER: ${funcName} successful`);
       return data.map(createModel);
     } catch (e: any) {
       console.log(
-        "SERVER ERROR: getAll failed:",
+        `SERVER ERROR: ${funcName} failed; `,
         e.message,
         e.response,
         e.request,
@@ -49,20 +50,56 @@ const getAll = async (setIsLoading: setIsLoadingT): Promise<Model[]> => {
   return safeRequest(setIsLoading, request);
 };
 
+// const getAllCategories = async (
+//   setIsLoading: setIsLoadingT,
+// ): Promise<Category[]> => {
+//   const axios = useAxios();
+//   const funcName = `getAllCategories`;
+//   console.log(`SERVER: trying ${funcName}`);
+
+//   const request = async () => {
+//     try {
+//       const { data } = await axios.get("/categories");
+
+//       const result = data.map((category: string) => {
+//         return { category, has_data: 0 };
+//       });
+//       console.log(`SERVER: ${funcName} successful`);
+//       return result;
+//     } catch (e: any) {
+//       console.log(
+//         `SERVER ERROR: ${funcName} failed; `,
+//         e.message,
+//         e.response,
+//         e.request,
+//       );
+//       throw e;
+//     }
+//   };
+
+//   return safeRequest(setIsLoading, request);
+// };
+
 const getById = async (
   setIsLoading: setIsLoadingT,
   id: number,
 ): Promise<Model> => {
-  console.log("SERVER: trying getById");
+  const funcName = `getById(${id})`;
+  console.log(`SERVER: trying ${funcName}`);
   const axios = useAxios();
 
   const request = async () => {
     try {
       const { data } = await axios.get(`/pet/${id}/`);
-      console.log("SERVER: getById successful");
+      console.log(`SERVER: ${funcName} successful`);
       return createModel(data);
     } catch (e: any) {
-      console.log("SERVER ERROR: getById failed:", e.toString());
+      console.log(
+        `SERVER ERROR: ${funcName} failed; `,
+        e.message,
+        e.response,
+        e.request,
+      );
       throw e;
     }
   };
@@ -71,16 +108,22 @@ const getById = async (
 };
 
 const add = async (setIsLoading: setIsLoadingT, obj_: Model): Promise<void> => {
-  console.log("SERVER: trying add");
   const axios = useAxios();
 
   const request = async () => {
-    const obj = removeOfflineKeys(obj_);
+    const { id: _, ...obj } = removeOfflineKeys(obj_);
+    const funcName = `add(${obj})`;
+    console.log(`SERVER: trying ${funcName}`);
     try {
       await axios.post("/pets", JSON.stringify(obj));
-      console.log("SERVER: add successful");
+      console.log(`SERVER: ${funcName} successful`);
     } catch (e: any) {
-      console.log("SERVER ERROR: add failed:", e.toString());
+      console.log(
+        `SERVER ERROR: ${funcName} failed; `,
+        e.message,
+        e.response,
+        e.request,
+      );
       throw e;
     }
   };
@@ -97,12 +140,19 @@ const update = async (
 
   const request = async () => {
     const obj = removeOfflineKeys(obj_);
+    const funcName = `update(${obj})`;
+    console.log(`SERVER: trying ${funcName}`);
     try {
       // TODO: check if not [patch] instead
       await axios.put(`/pet/${obj.id}/`, JSON.stringify(obj));
-      console.log("SERVER: update successful");
+      console.log(`SERVER: ${funcName} successful`);
     } catch (e: any) {
-      console.log("SERVER ERROR: update failed:", e.toString());
+      console.log(
+        `SERVER ERROR: ${funcName} failed; `,
+        e.message,
+        e.response,
+        e.request,
+      );
       throw e;
     }
   };
@@ -114,15 +164,21 @@ const remove = async (
   setIsLoading: setIsLoadingT,
   id: number,
 ): Promise<void> => {
-  console.log("SERVER: trying remove");
+  const funcName = `remove(${id})`;
+  console.log(`SERVER: trying ${funcName}`);
   const axios = useAxios();
 
   const request = async () => {
     try {
       await axios.delete(`/pet/${id}/`);
-      console.log("SERVER: remove successful");
+      console.log(`SERVER: ${funcName} successful`);
     } catch (e: any) {
-      console.log("SERVER ERROR: remove failed:", e.toString());
+      console.log(
+        `SERVER ERROR: ${funcName} failed; `,
+        e.message,
+        e.response,
+        e.request,
+      );
       throw e;
     }
   };
@@ -131,17 +187,18 @@ const remove = async (
 };
 
 const search = async (setIsLoading: setIsLoadingT): Promise<Model[]> => {
-  console.log("SERVER: trying search");
+  const funcName = `search`;
+  console.log(`SERVER: trying ${funcName}`);
   const axios = useAxios();
 
   const request = async () => {
     try {
       const { data } = await axios.get("/search");
-      console.log("SERVER: search successful");
+      console.log(`SERVER: ${funcName} successful`);
       return data.map(createModel);
     } catch (e: any) {
       console.log(
-        "SERVER ERROR: search failed:",
+        `SERVER ERROR: ${funcName} failed; `,
         e.message,
         e.response,
         e.request,
