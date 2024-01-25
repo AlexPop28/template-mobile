17a91c6183eac52358106ec564aeb22e5c98a281
Alex Pop <popalexnicolae@gmail.com>
2024-01-25 07:28:20 +0200

    Update progress indicator

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,21 +1,26 @@
 import React, { ReactNode } from "react";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import { useRepository } from "../lib/RepositoryContext";
 import { sharedStyles } from "../screens/styles";
 
 interface ConditionalWrapperProps {
-  isAvailable: boolean;
-  children: ReactNode;
+    isAvailable: boolean;
+    children: ReactNode;
 }
 
 const Container: React.FC<ConditionalWrapperProps> = ({
-  isAvailable,
-  children,
+    isAvailable,
+    children,
 }) => {
-  const repository = useRepository();
-  return (
-    <View style={sharedStyles.container}>
-      {repository.isLoading && <Text>Loading...</Text>}
+    const repository = useRepository();
+    return (
+        <View style={sharedStyles.container}>
+      {repository.isLoading && (
+        <>
+          <ActivityIndicator animating={true} color="#007AFF" size="large" />
+          <Text>Loading...</Text>
+        </>
+      )}
       {!repository.isLoading && (
         <>
           {isAvailable && children}
